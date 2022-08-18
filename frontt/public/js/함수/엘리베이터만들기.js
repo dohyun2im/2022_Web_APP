e8034300459240b103e2fb67762da49c5ad0fff8
504 <504@DESKTOP-ECPN2C5>
2022-08-18 18:14:38 +0900

    123

--- frontt/public/js/함수/엘리베이터만들기.js
+++ frontt/public/js/함수/엘리베이터만들기.js
@@ -3,40 +3,61 @@ function 층이동1(){
 	document.querySelector('.elevator').style.cssText  = 'top:680px';
 	document.querySelector('.peaple').style.cssText  = 'top:680px';
 	document.getElementById('floorcon').innerHTML = '1층';
+	if(confirm("내리시겠습니까 ?")){
+		document.querySelector('.peaple').remove();
+	}else{}
 }
 
 function 층이동2(){
 	document.querySelector('.elevator').style.cssText  = 'top:560px';
 	document.querySelector('.peaple').style.cssText  = 'top:560px';
 	document.getElementById('floorcon').innerHTML = '2층';
+	if(confirm("내리시겠습니까 ?")){
+		document.querySelector('.peaple').remove();
+	}else{}
 }
 
 function 층이동3(){
 	document.querySelector('.elevator').style.cssText  = 'top:450px';
 	document.querySelector('.peaple').style.cssText  = 'top:450px';
 	document.getElementById('floorcon').innerHTML = '3층';
+	if(confirm("내리시겠습니까 ?")){
+		document.querySelector('.peaple').remove();
+	}else{}
 }
 
 function 층이동4(){
 	document.querySelector('.elevator').style.cssText  = 'top:340px';
 	document.querySelector('.peaple').style.cssText  = 'top:340px';
 	document.getElementById('floorcon').innerHTML = '4층';
+	if(confirm("내리시겠습니까 ?")){
+		document.querySelector('.peaple').remove();
+	}else{}
 }
 
 function 층이동5(){
 	document.querySelector('.elevator').style.cssText  = 'top:230px';
 	document.querySelector('.peaple').style.cssText  = 'top:230px';
 	document.getElementById('floorcon').innerHTML = '5층';
+	if(confirm("내리시겠습니까 ?")){
+		document.querySelector('.peaple').remove();
+	}else{}
 }
 
 function 층이동6(){
 	document.querySelector('.elevator').style.cssText  = 'top:120px';
 	document.querySelector('.peaple').style.cssText  = 'top:120px';
 	document.getElementById('floorcon').innerHTML = '6층';
+	if(confirm("내리시겠습니까 ?")){
+		document.querySelector('.peaple').remove();
+	}else{}
 }
 
 function 층이동7(){
 	document.querySelector('.elevator').style.cssText  = 'top:10px';
 	document.querySelector('.peaple').style.cssText  = 'top:10px';
 	document.getElementById('floorcon').innerHTML = '7층';
+	if(confirm("내리시겠습니까 ?")){
+		document.querySelector('.peaple').remove();
+	}else{}
 }
